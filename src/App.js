52ce5512e5b9c6995d793ad31fb100491b02ee03
DJN1 <david.niederweis@gmail.com>
2019-02-25 13:40:54 -0600

    implemented vigenere

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '../public/static/css/bootstrap.css'
+import { Caesar } from "./Caesar"
+import { Vigenere } from "./Vigenere"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container" id="mainContainer">
+          <label>Caesar Cipher<Caesar /></label>
+          <label>Caesar Cipher Output: <p></p></label>
+
+          <Vigenere />
+        </div>
       </div>
     );
   }
